52a4d991e61875a7edd2f24725f77e10af06c7da
Harsh <harshshrivastava333@gmail.com>
2021-05-19 21:39:47 +0530

    Initial commit

---
+++ js/index.js
@@ -0,0 +1,38 @@
+ScrollReveal().reveal('section', {
+    interval: 30,
+    reset: false
+});
+ScrollReveal().reveal('header', {
+    interval: 30,
+    reset: false
+});
+ScrollReveal().reveal('footer', {
+    interval: 20,
+    reset: false
+});
+
+const navToggle = document.querySelector('.nav-toggle');
+const navLink = document.querySelectorAll('.nav__link');
+
+navToggle.addEventListener('click', () => {
+    document.body.classList.toggle('nav-open');
+});
+
+navLink.forEach(link => {
+    link.addEventListener('click', () => {
+        document.body.classList.toggle('nav-open');
+    });
+});
+
+let currentSlide = 0;
+function moveslide(direction) {
+    currentSlide = modulo(currentSlide + direction, 10);
+
+    const image = document.querySelectorAll('.image');
+    image.forEach(images => images.style.display = "none");
+    image[currentSlide].style.display = "flex";
+}
+
+function modulo(num, mod) {
+    return (num % mod + mod) % mod;
+}
